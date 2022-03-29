2fa5fcc30fafd9687b613843214acbb5eda5051e
JoseF <jose.tf2396@gmail.com>
2022-03-28 20:10:50 -0700

    Text Input

---
+++ components/textInput.sytles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  textInputStyles: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 30,
+    paddingHorizontal: 17,
+  },
+});
